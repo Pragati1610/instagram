81c91ba8e1ee2daf962453ae2eebb1fa528494f6
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-11 17:57:07 +0530

    Update

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ let checkURL = function (item) {
     let regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
 
     if (part.match(regexWithoutHttp) || part.match(regexWithHttp)) {
-      item["url"] = part;
+      part = part.split(/[\r\n]+/gm)
+      item["url"] = part[0];
     }
   });
 };
