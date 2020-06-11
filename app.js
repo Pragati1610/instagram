78c2516718a6ae94cf8380999f1960921813ed1b
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-11 17:25:22 +0530

    Update

--- app.js
+++ app.js
@@ -12,59 +12,54 @@ const client = redis.createClient(process.env.REDIS_URL);
 app.use(cors());
 
 client.on("error", function (error) {
-    console.error(error);
+  console.error(error);
 });
 
 const url = `https://graph.facebook.com/${process.env.INSTAGRAM_ID}/media?fields=id,media_type,media_url,timestamp,caption&access_token=${process.env.ACCESS_TOKEN}`;
 
-let getURL = function (item) {
-    let caption = item.caption;
-    caption.split(" ").forEach((part) => {
-        let regexWithoutHttp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-        let regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+let checkURL = function (item) {
+  let caption = item.caption;
+  caption.split(" ").forEach((part) => {
+    let regexWithoutHttp = /[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
+    let regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
 
-        if (part.match(regexWithoutHttp) || part.match(regexWithHttp)) {
-            item["url"] = part;
-        }
-
-    });
-}
-
-let checkURL1 = function (item) {
-    let caption = item.caption;
-    item["url"] = getURL(caption);
-}
+    if (part.match(regexWithoutHttp) || part.match(regexWithHttp)) {
+      item["url"] = part;
+    }
+  });
+};
 
 app.get("/", async (req, res) => {
-    client.get("completeData", async (err, reply) => {
-        if (reply) {
-            reply = JSON.parse(reply)
-            res.json(reply);
-        } else {
-            try {
-                let result = await axios.get(url);
-                let data = result.data.data;
-                data.forEach(checkURL1);
-
-                client.set("completeData", JSON.stringify({
-                        data: data
-                    }),
-                    "EX",
-                    7 * 60 * 60
-                );
-                return res.status(200).json({
-                    data: data
-                });
-            } catch (err) {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            }
-        }
-    })
+  client.get("completeData", async (err, reply) => {
+    if (reply) {
+      reply = JSON.parse(reply);
+      res.json(reply);
+    } else {
+      try {
+        let result = await axios.get(url);
+        let data = result.data.data;
+        data.forEach(checkURL);
+        client.set(
+          "completeData",
+          JSON.stringify({
+            data: data,
+          }),
+          "EX",
+          7 * 60 * 60
+        );
+        return res.status(200).json({
+          data: data,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      }
+    }
+  });
 });
 
 app.listen(port, (req, res) => {
-    console.log("Sever started");
+  console.log("Sever started");
 });
